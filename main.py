bf33285941c9843b2534ca8334bd7c8cd27641d2
Oscar C <oscar.carrillo758@gmail.com>
2024-02-04 17:42:28 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-#1Create a greeting for your program.
+#1. Create a greeting for your program.
 print("Welcome to the band generator.")
 
 #2. Ask the user what their name is.
 print ('Hello ' + input ("What is your name?\n"))
 
-#2. Ask the user for the city that they grew up in.
+#3. Ask the user for the city that they grew up in.
 city = input("What was the city you grew up in?\n")
 print (city)
 
-#3. Ask the user for the name of a pet.
+#4. Ask the user for the name of a pet.
 pet = input ("What is the name of your pet?\n")
 print (pet)
 
-#4. Combine the name of their city and pet and show them their band name.
+#5. Combine the name of their city and pet and show them their band name.
 print ("Your band name could be " + city + " " + pet)
 
 
