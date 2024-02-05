d588da9e26db9971541c314326e748db1b62ce4a
Oscar C <oscar.carrillo758@gmail.com>
2024-02-04 17:41:50 -0800

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,5 @@ print (pet)
 #4. Combine the name of their city and pet and show them their band name.
 print ("Your band name could be " + city + " " + pet)
 
-#5. Make sure the input cursor shows on a new line:
-#add \n to create new lines.
 
 
-# Solution: https://replit.com/@appbrewery/band-name-generator-end
